d1143d1316725b5d58c848a8d600850adf571edb
Matt Ingram <mattingram0@gmail.com>
2017-12-25 01:13:45 +0000

    Deleted gkxx72

--- gkxx72/gkxx72rest/AStar/Vertex.py
+++
@@ -1,5 +0,0 @@
-class Vertex:
-
-    def __init__(self, name, dist):
-        self.name = name
-        self.dist = dist
--- gkxx72/gkxx72rest/Experimentation/Greedy/Node.py
+++
@@ -1,9 +0,0 @@
-class Node:
-
-    def __init__(self, name, dist):
-        self.name = name
-        self.dist = dist
-
-    def __cmp__(self, other):
-        return cmp(self.dist, other.dist)
-
